283732d376a6c211da053eb843ef3649523b3e8b
Yeesa Kee <yeesakee99@gmail.com>
2022-09-19 16:14:01 -0700

    Fixed empty list bug. All problems completed and tested! :)

--- Assignment_1/HW1.hs
+++ Assignment_1/HW1.hs
@@ -1,6 +1,6 @@
 -- CptS 355 - Fall 2022 -- Homework1 - Haskell
 -- Name: Yeesa Kee
--- Collaborators: ---
+-- Collaborators: None
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use camelCase" #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
@@ -16,17 +16,17 @@ import Data.Char (digitToInt, intToDigit)
 -- P1 - merge_sorted 10%
 -- merge_sorted: takes two sorted lists and returns a sorted merged list
 merge_sorted :: Ord a => [a] -> [a] -> [a]
-merge_sorted [] [] = []    -- base case: if both lists are empty return empty list
-merge_sorted [] y = y      -- base case: if first list is empty return second list y
-merge_sorted x [] = x      -- base case: if second list is empty return first list x
-merge_sorted (x:xs) (y:ys) | x <= y = x: merge_sorted xs (y:ys)       -- if x is less than or equal to y then concat x
-                           | otherwise = y:merge_sorted (x:xs) ys     -- otherwise concat y
+merge_sorted [] [] = []
+merge_sorted [] y = y
+merge_sorted x [] = x
+merge_sorted (x:xs) (y:ys) | x <= y = x: merge_sorted xs (y:ys)    -- if x is less than or equal to y then concat x
+                           | otherwise = y:merge_sorted (x:xs) ys  -- otherwise concat y
 
 -- P2  sum_range  15%
 -- sum_range: takes a tuple representing indices, a list of numbers, and return the sum of the
 --            numbers in between the given indices. (Inclusive of the beginning and end indicies)
 sum_range :: (Ord a, Num p, Num a) => (a, a) -> [p] -> p
-sum_range (a,b) [] = 0   -- base case: if list is empty return 0
+sum_range (a,b) [] = 0
 sum_range tp iL = sum_range_helper tp iL 0
      where
           sum_range_helper (a,b) (x:xs) index
@@ -38,8 +38,8 @@ sum_range tp iL = sum_range_helper tp iL 0
 -- P3  (a) calc_collatz_seq ; 10%
 -- calc_collatz_seq: takes a numer and returns a list of the collatz sequence of the given number
 calc_collatz_seq :: Integral a => a -> [a]
-calc_collatz_seq 0 = [1]    -- base case: if num is 0 return [1]
-calc_collatz_seq 1 = [1]    -- base case: if num is 1 return [1]
+calc_collatz_seq 0 = [1]
+calc_collatz_seq 1 = [1]
 calc_collatz_seq num
      -- if num is even then concat num and call function with num/2
      | (num `mod` 2 == 0) = num:calc_collatz_seq (num `div` 2)
@@ -49,8 +49,8 @@ calc_collatz_seq num
 -- P3  (b) longest_collatz_seq ; 15%
 -- longest_collatz_seq: takes an integer and returns a list of integers
 longest_collatz_seq :: Integral a => a -> [a]
-longest_collatz_seq 0 = [1]    -- base case: if num is 0 return [1]
-longest_collatz_seq 1 = [1]    -- base case: if num is 1 return [1]
+longest_collatz_seq 0 = [1]
+longest_collatz_seq 1 = [1]
 longest_collatz_seq num =
      longest_collatz_seq_helper (calc_collatz_seq num) (calc_collatz_seq (num-1)) num (num-1)
      where
@@ -59,9 +59,9 @@ longest_collatz_seq num =
           -- takes two integers, xnum and ynum, represents the collatz sequence of xL and yL
           -- returns the list that has the longest collatz sequence
           longest_collatz_seq_helper :: Integral a => [a] -> [a] -> a -> a -> [a]
-          longest_collatz_seq_helper [] [] xnum ynum = []    -- base case: if both lists are empty return empty list
-          longest_collatz_seq_helper xL [] xnum ynum = xL    -- base case: if second list is empty return first list xL
-          longest_collatz_seq_helper [] yL xnum ynum = yL    -- base case: if first list is empty return second list yL
+          longest_collatz_seq_helper [] [] xnum ynum = []
+          longest_collatz_seq_helper xL [] xnum ynum = xL
+          longest_collatz_seq_helper [] yL xnum ynum = yL
           longest_collatz_seq_helper xL yL xnum ynum
                | length(xL) >= length(yL) =
                     if ynum <= 0 then xL    -- no more numbers to check
@@ -79,7 +79,7 @@ longest_collatz_seq num =
 --              example list: wsu_games = [(2019, [("NMSU",(58,7)), ("UNCO",(59,17))]
 --              returns all the scores played against the given school
 game_scores :: Eq t => [(a1, [(t, a2)])] -> t -> [a2]
-game_scores [] target_school = []    -- base case: if list is empty return empty list
+game_scores [] target_school = []
 game_scores (x:xs) target_school =
      -- call game_scores_by_year and concat game_scores of the rest of the list
      game_scores_by_year x target_school ++ game_scores xs target_school
@@ -102,7 +102,7 @@ game_scores (x:xs) target_school =
 --               example list: wsu_games = [(2019, [("NMSU",(58,7)), ("UNCO",(59,17))]
 --               returns a list of tuples in the format: [(year, # wins)]
 wins_by_year :: (Num b, Ord a1) => [(a2, [(a3, (a1, a1))])] -> [(a2, b)]
-wins_by_year [] = []     -- base case: if list is empty return empty list
+wins_by_year [] = []
 wins_by_year (x:xs) = (fst(x), wins_by_year_helper x) : wins_by_year xs
      where
           -- wins_by_year_helper: takes a tuple
--- Assignment_1/HW1Tests.hs
+++ Assignment_1/HW1Tests.hs
@@ -13,9 +13,9 @@ import HW1
 import HW1 (merge_sorted, wins_by_year)
 
 -- P1. merge_sorted tests
--- merge_sorted_test1 = TestCase (assertEqual "merge_sorted_test1"
---                                             []
---                                             (merge_sorted [] []))
+merge_sorted_test1 = TestCase (assertEqual "merge_sorted_test1"
+                                            ([]::[Int])
+                                            (merge_sorted [] []))
 merge_sorted_test2 = TestCase (assertEqual "merge_sorted_test2"
                                             [1,2,3]
                                             (merge_sorted [1,2,3] []))
@@ -60,7 +60,9 @@ calc_collatz_seq_test3 = TestCase (assertEqual "calc_collatz_seq_test3"
                                   [12,6,3,10,5,16,8,4,2,1] 
                                   (calc_collatz_seq 12) ) 
 calc_collatz_seq_test4 = TestCase (assertEqual "calc_collatz_seq_test4" 
-                                  [123,370,185,556,278,139,418,209,628,314,157,472,236,118,59,178,89,268,134,67,202,101,304,152,76,38,19,58,29,88,44,22,11,34,17,52,26,13,40,20,10,5,16,8,4,2,1]  
+                                  [123,370,185,556,278,139,418,209,628,314,157,472,236,118,59,
+                                  178,89,268,134,67,202,101,304,152,76,38,19,58,29,88,44,22,11,
+                                  34,17,52,26,13,40,20,10,5,16,8,4,2,1]  
                                   (calc_collatz_seq 123) ) 
 
 -- (b) longest_collatz_seq tests
@@ -74,7 +76,22 @@ longest_collatz_seq_test3 = TestCase (assertEqual "longest_collatz_seq3"
                                   [19,58,29,88,44,22,11,34,17,52,26,13,40,20,10,5,16,8,4,2,1]
                                   (longest_collatz_seq 20) )
 longest_collatz_seq_test4 = TestCase (assertEqual "longest_collatz_seq4" 
-                                  [97,292,146,73,220,110,55,166,83,250,125,376,188,94,47,142,71,214,107,322,161,484,242,121,364,182,91,274,137,412,206,103,310,155,466,233,700,350,175,526,263,790,395,1186,593,1780,890,445,1336,668,334,167,502,251,754,377,1132,566,283,850,425,1276,638,319,958,479,1438,719,2158,1079,3238,1619,4858,2429,7288,3644,1822,911,2734,1367,4102,2051,6154,3077,9232,4616,2308,1154,577,1732,866,433,1300,650,325,976,488,244,122,61,184,92,46,23,70,35,106,53,160,80,40,20,10,5,16,8,4,2,1]
+                                  [27,82,41,124,62,31,94,47,142,71,214,107,322,161,484,242,121,
+                                  364,182,91,274,137,412,206,103,310,155,466,233,700,350,175,526,
+                                  263,790,395,1186,593,1780,890,445,1336,668,334,167,502,251,754,
+                                  377,1132,566,283,850,425,1276,638,319,958,479,1438,719,2158,
+                                  1079,3238,1619,4858,2429,7288,3644,1822,911,2734,1367,4102,2051,
+                                  6154,3077,9232,4616,2308,1154,577,1732,866,433,1300,650,325,976,
+                                  488,244,122,61,184,92,46,23,70,35,106,53,160,80,40,20,10,5,16,8,4,2,1]
+                                  (longest_collatz_seq 27) )                        
+longest_collatz_seq_test5 = TestCase (assertEqual "longest_collatz_seq5" 
+                                  [97,292,146,73,220,110,55,166,83,250,125,376,188,94,47,142,71,214,
+                                  107,322,161,484,242,121,364,182,91,274,137,412,206,103,310,155,466,
+                                  233,700,350,175,526,263,790,395,1186,593,1780,890,445,1336,668,334,
+                                  167,502,251,754,377,1132,566,283,850,425,1276,638,319,958,479,1438,
+                                  719,2158,1079,3238,1619,4858,2429,7288,3644,1822,911,2734,1367,4102,
+                                  2051,6154,3077,9232,4616,2308,1154,577,1732,866,433,1300,650,325,976,
+                                  488,244,122,61,184,92,46,23,70,35,106,53,160,80,40,20,10,5,16,8,4,2,1]
                                   (longest_collatz_seq 100) )
 
 -- P4. (a) game_scores and (b) wins_by_year tests  (one test is sufficient for wins_by_year)                                
@@ -87,24 +104,44 @@ wsu_games = [
             ("ORST",(31,24)), ("STAN",(34,31)), ("BYU",(19,21)), ("ASU",(34,21)), ("ORE",(24,38)), 
             ("ARIZ",(44,18)), ("WASH",(40,13)), ("CMU",(21,24))] )
             ]
+wsu_games_modified = [
+    (2020, [("NMSU",(58,7)), ("UNCO",(59,17)), ("HOU",(31,24)), ("UCLA",(67,67)), ("UTAH",(13,38)), 
+            ("ASU",(34,38)), ("COLO",(41,10)), ("ORE",(35,37)), ("CAL",(20,33)), ("STAN",(49,22)), 
+            ("ORST",(54,53)), ("WASH",(13,31)), ("AFA",(21,31))]),
+    (2021, [("ORST",(18,28)), ("ORE",(29,43)), ("USC",(13,38)), ("UTAH",(28,45))]),
+    (2022, [("USU",(23,26)), ("PORT ST.",(44,24)), ("USC",(14,45)), ("UTAH",(13,24)), ("CAL",(21,6)),
+            ("ORST",(31,24)), ("STAN",(34,31)), ("BYU",(19,21)), ("ASU",(34,21)), ("ORE",(24,38)), 
+            ("ARIZ",(44,18)), ("WASH",(40,13)), ("CMU",(21,24))] )
+            ]
+wsu_games_2019 = [
+    (2019, [("NMSU",(58,7)), ("UNCO",(59,17)), ("HOU",(31,24)), ("UCLA",(63,67)), ("UTAH",(13,38)), 
+            ("ASU",(34,38)), ("COLO",(41,10)), ("ORE",(35,37)), ("CAL",(20,33)), ("STAN",(49,22)), 
+            ("ORST",(54,53)), ("WASH",(13,31)), ("AFA",(21,31))])
+            ]
+wsu_games_empty = []::[(Int, [([Char], (Int, Int))])]
             
 game_scores_test1 = TestCase (assertEqual "game_scores_test1" 
                                   []
-                                  (sort $ game_scores wsu_games "BERKLEY") ) 
+                                  (sort $ game_scores wsu_games_empty "BERKLEY") ) 
 game_scores_test2 = TestCase (assertEqual "game_scores_test2" 
                                   (sort [(63,67)]) 
                                   (sort $ game_scores wsu_games "UCLA") ) 
 game_scores_test3 = TestCase (assertEqual "game_scores_test3" 
                                   (sort [(13,38),(28,45),(13,24)])
                                   (sort $ game_scores wsu_games "UTAH") ) 
+game_scores_test4 = TestCase (assertEqual "game_scores_test4" 
+                                  (sort [(13,38)])
+                                  (sort $ game_scores wsu_games_2019 "UTAH") )
 
-
--- wins_by_year_test1 = TestCase (assertEqual "wins_by_year_test1" 
---                                   []  
---                                   (sort $ wins_by_year []) )  
+wins_by_year_test1 = TestCase (assertEqual "wins_by_year_test1" 
+                                  []
+                                  (sort $ wins_by_year wsu_games_empty) )  
 wins_by_year_test2 = TestCase (assertEqual "wins_by_year_test2" 
-                                  (sort [(2019,6),(2020,1),(2021,7)])  
-                                  (sort $ wins_by_year wsu_games) )                   
+                                  (sort [(2019,5),(2020,0),(2021,7)])  
+                                  (sort $ wins_by_year wsu_games_modified) )       
+wins_by_year_test3 = TestCase (assertEqual "wins_by_year_test3" 
+                                  (sort [(2019,6)])  
+                                  (sort $ wins_by_year wsu_games_2019) )                                              
 -- P5. compress_str tests
 
 compress_str_test1 = TestCase (assertEqual "(compress_str_test1)" 
@@ -121,7 +158,9 @@ compress_str_test4 = TestCase (assertEqual "(compress_str_test4)"
                                   (compress_str "abbbcdeffggg") )    
 
 -- add the test cases you created to the below list. 
-tests = TestList [ --TestLabel "merge_sorted- test1 " merge_sorted_test1,
+tests = TestList [ 
+                   -- Problem 1 tests: merge_sorted
+                   TestLabel "merge_sorted- test1 " merge_sorted_test1,
                    TestLabel "merge_sorted- test2 " merge_sorted_test2,  
                    TestLabel "merge_sorted- test3 " merge_sorted_test3,
                    TestLabel "merge_sorted- test4 " merge_sorted_test4,
@@ -129,28 +168,36 @@ tests = TestList [ --TestLabel "merge_sorted- test1 " merge_sorted_test1,
                    TestLabel "merge_sorted- test6 " merge_sorted_test6,
                    TestLabel "merge_sorted- test7 " merge_sorted_test7,
 
+                   -- Problem 2 tests: sum_range
                    TestLabel "sum_range- test1 " sum_range_test1,
                    TestLabel "sum_range- test2 " sum_range_test2,  
                    TestLabel "sum_range- test3 " sum_range_test3,
                    TestLabel "sum_range- test4 " sum_range_test4,
-
+                   
+                   -- Problem 3a tests: calc_collatz_seq
                    TestLabel "calc_collatz_seq- test1 " calc_collatz_seq_test1, 
                    TestLabel "calc_collatz_seq- test2 " calc_collatz_seq_test2, 
                    TestLabel "calc_collatz_seq- test3 " calc_collatz_seq_test3,
                    TestLabel "calc_collatz_seq- test4 " calc_collatz_seq_test4,
-
+                   
+                   -- Problem 3b tests: longest_collatz_seq
                    TestLabel "longest_collatz_seq- test1 " longest_collatz_seq_test1, 
                    TestLabel "longest_collatz_seq- test2 " longest_collatz_seq_test2, 
                    TestLabel "longest_collatz_seq- test3 " longest_collatz_seq_test3,
-                   TestLabel "longest_collatz_seq- test4 " longest_collatz_seq_test1,
+                   TestLabel "longest_collatz_seq- test4 " longest_collatz_seq_test4,
+                   TestLabel "longest_collatz_seq- test5 " longest_collatz_seq_test5,
 
+                   -- Problem 4a tests: game_scores
                    TestLabel "game_scores- test1 " game_scores_test1, 
                    TestLabel "game_scores- test2 " game_scores_test2, 
                    TestLabel "game_scores- test3 " game_scores_test3,
                    
-                   --TestLabel "wins_by_year- test1 " game_scores_test1,
+                   -- Problem 4b tests: wins_by_year
+                   TestLabel "wins_by_year- test1 " game_scores_test1,
                    TestLabel "wins_by_year- test2 " game_scores_test2,
+                   TestLabel "wins_by_year- test3 " game_scores_test3,
 
+                   -- Problem 5 tests: compress_str
                    TestLabel "compress_str- test1" compress_str_test1,
                    TestLabel "compress_str- test2" compress_str_test2,
                    TestLabel "compress_str- test3" compress_str_test3,
